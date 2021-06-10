424ecb93ba3a7d719d2a9945feb9c6ceb89deac9
Alexios Philalithis <46726143+Alexios99@users.noreply.github.com>
2021-06-10 19:07:30 +0100

    Update capture_test.py

--- Human Emotion Detection AI/capture_test.py
+++ Human Emotion Detection AI/capture_test.py
@@ -2,7 +2,7 @@ import cv2
 def webcam():
     cam = cv2.VideoCapture(0)
 
-    cv2.namedWindow("test")
+    cv2.namedWindow("Webcam")
 
     img_counter = 0
 
@@ -15,11 +15,9 @@ def webcam():
 
         k = cv2.waitKey(1)
         if k%256 == 27:
-            # ESC pressed
             print("Escape hit, closing...")
             break
         elif k%256 == 32:
-            # SPACE pressed
             img_name = "opencv_frame_{}.png".format(img_counter)
             cv2.imwrite(img_name, frame)
             print("{} written!".format(img_name))
